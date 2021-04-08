3d3e1c5741889a78ecd32d9bf9c58f3e52f18d65
Claudio <claudiocarchietti@yahoo.it>
2021-04-08 16:32:03 +0200

    Esercizio terminato,con bonus, e aggiunta di comando in mounted
    Aggiunto comando $nextTick al mounted,che stampa quando il doc è pronto

--- main.js
+++ main.js
@@ -11,17 +11,15 @@ var app = new Vue ({
     email: []
   },
   mounted: function () {
-    // this.$nextTick(function () {
-    //   // Code that will run only after the
-    //   // entire view has been rendered
-    //   for (var i = 0; i < 10; i++) {
-    //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    //     .then((response) => {
-    //       let emailgen = response.data.response;
-    //       this.email.push(emailgen);
-    //     });
-    //   }
-    // })
+    this.$nextTick(function () {
+      for (var i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          let emailgen = response.data.response;
+          this.email.push(emailgen);
+        });
+      }
+    })
   },
   methods: {
     emailRand: function () {
